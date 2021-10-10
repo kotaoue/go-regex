80a3165eae798d85051c07b3d26aabf9e108140e
kota oue <kota.oue@yappli.co.jp>
2021-10-10 20:58:24 +0900

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -465,23 +465,27 @@ func Test_Dollar(t *testing.T) {
 	}
 }
 
-func Test_LowerB(t *testing.T) {
+func Test_UpperB(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected bool
 	}{
 		{
-			input:    "book",
+			input:    "mac",
+			expected: false,
+		},
+		{
+			input:    "emacs",
 			expected: true,
 		},
 		{
-			input:    "bookshelf",
+			input:    "macintosh",
 			expected: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
-			r := regexp.MustCompile(`\bbook\b`)
+			r := regexp.MustCompile(`\Bmac\B`)
 			assert.Equal(t, tt.expected, r.MatchString(tt.input))
 		})
 	}
